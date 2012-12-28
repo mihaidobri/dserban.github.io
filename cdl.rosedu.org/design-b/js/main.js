fb7df2cd778aede6da317ef87b36c129f0949e90
Dan Serban <dserban01@gmail.com>
2012-12-28 19:08:16 +0200

    CDL redesign: preloading of arrow-hover and green-gradient

--- cdl.rosedu.org/design-b/js/main.js
+++ cdl.rosedu.org/design-b/js/main.js
@@ -24,16 +24,20 @@ window.addEventListener('popstate', function(event) {
 Zepto(function($) {
   history.pushState("#acasa_content", "/", window.location.href);
   var all_images = {
-    "#cdl_logo":      "images/cdl.png",
-    "#rosedu_logo":   "images/rosedu.png",
-    "#ixia_logo":     "images/ixia.png",
-    "#eaudeweb_logo": "images/eaudeweb.png",
-    "#google_logo":   "images/google.png",
-    "#english_logo":  "images/english.png",
-    "#alexef_pic":    "images/alexef.png"
+    "#cdl_logo":       "images/cdl.png",
+    "#rosedu_logo":    "images/rosedu.png",
+    "#ixia_logo":      "images/ixia.png",
+    "#eaudeweb_logo":  "images/eaudeweb.png",
+    "#google_logo":    "images/google.png",
+    "#english_logo":   "images/english.png",
+    "#arrow_hover":    "images/arrow-hover.png",
+    "#green_gradient": "images/green-gradient.png",
+    "#alexef_pic":     "images/alexef.png"
   }
   for (var img_id in all_images) {
     $(img_id).attr("src", all_images[img_id]);
   }
+  var iframe_url = "http://www.google.com/calendar/embed?src=apve67v2o1l4tp1655sl53nhs8%40group.calendar.google.com&ctz=Europe/Bucharest&bgcolor=%23F3F3F3"
+  $("#calendar_iframe").attr("src", iframe_url);
 });
 
