c3ebfe3a72bb57c17f0dfda90cd793c21478c434
Dan Serban <dserban01@yahoo.com>
2013-08-22 17:56:24 +0300

    Update validity_of_media_url.rb

--- tmp/validity_of_media_url.rb
+++ tmp/validity_of_media_url.rb
@@ -33,3 +33,19 @@ end
 
 # 403 text/html http://s1.fisierulmeu.ro:32681/dwn_links/600876f7472fca49a68c638d6bb801fc/4f8f022f/hd2/41/41WXZE0JM49N/name/%5Bwww.fisierulmeu.ro%5D%20Parazitii%20-%20Nu%20ma%20schimbi.mp3
 
+require 'curb'
+
+# url = 'http://ekiga.net/ip/'
+url = 'http://www.hulkshare.com/ap-h1wcbkkcszy8.mp3'
+# url = ''
+# url = ''
+# url = ''
+
+c = Curl::Easy.http_head(url) do |curl|
+  curl.headers["User-Agent"] = "Mozilla/5.0"
+  curl.follow_location = true
+  curl.enable_cookies = true
+end
+
+puts "http_code:    #{c.response_code}"
+puts "content_type: #{c.content_type}"
